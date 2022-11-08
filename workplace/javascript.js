3675df8ce1172ff720c4ca6ba190c3bac54e18f0
Alexis <114521164+Alexislii@users.noreply.github.com>
2022-11-08 05:51:57 -0500

    java and other changes

---
+++ workplace/javascript.js
@@ -0,0 +1,4 @@
+const myClickMsg = document.getElementsByClassName("git_btn");
+function myClick() { window.alert("Hi! Thanks for the connection. I will get back to to you in 1 or 2 business days.") }
+const myClickMsg2 = document.getElementById("appointment");
+function myClick2() { window.alert("Hi my friend! I have received your appointment request. See you soon!") }
